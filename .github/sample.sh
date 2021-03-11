4687e92e739ec405801c93b4078ce950ec5b76a5
Ryosuke Uchitate <ruchitate@coiney.com>
2021-03-11 18:02:19 +0900

    Add

---
+++ .github/sample.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+declare -A musics=(
+        ["ultra soul"]="2001"
+        ["OCEAN"]="2005"
+        ["BANZAI"]="2004"
+        ["RED"]="2015"
+        ["GIFT"]="2008"
+)
+
+echo "${musics["ultra soul"]}"
